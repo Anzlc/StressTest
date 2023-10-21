e6a45c00bd22ed73e34e0fca1838a755e0ac2849
Anzlc <107500985+Anzlc@users.noreply.github.com>
2023-10-21 14:07:14 +0200

    Initial commit plus Cpu stress test implementation

---
+++ CPU/CpuStressTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StressTest.CPU
+{
+    public class CpuStressTest
+    {
+
+        public void Stress()
+        {
+            Console.WriteLine("Starting...");
+
+            
+
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                Console.WriteLine($"Thread {i}# started!");
+                Thread thread = new Thread(new ThreadStart(GeneratePrimeNumbers));
+                thread.Start();
+            }
+
+            GeneratePrimeNumbers();
+        }
+
+        internal void GeneratePrimeNumbers() {
+            int i = 1;
+            int primeNumbersCount = 0;
+            while (true)
+            {
+               
+                int dividerCount = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    { 
+                        dividerCount++;
+                    }
+                }
+                if (dividerCount == 2)
+                {
+                    primeNumbersCount++;
+
+
+                }
+                i++;
+            }
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using StressTest.CPU;
+using System;
+
+namespace StressTest 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CpuStressTest cpuStressTest = new CpuStressTest();
+
+            cpuStressTest.Stress();
+        }
+    }
+}
